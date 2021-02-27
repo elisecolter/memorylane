99056743d4d9c9ab7f0f751f18a9e310212d3252
Elise Colter <elisecolter@gmail.com>
2021-02-27 15:25:37 -0500

    Integrate Leaflet

--- app/views.py
+++ app/views.py
@@ -10,33 +10,33 @@ def index():
 	return render_template('index.html')
 
 
-@app.route('/list/')
-def posts():
-	return render_template('list.html')
+# @app.route('/list/')
+# def posts():
+# 	return render_template('list.html')
 
 
-@app.route('/new/')
-@login_required
-def new():
-	form = ExampleForm()
-	return render_template('new.html', form=form)
+# @app.route('/new/')
+# @login_required
+# def new():
+# 	form = ExampleForm()
+# 	return render_template('new.html', form=form)
 
 
-@app.route('/save/', methods = ['GET','POST'])
-@login_required
-def save():
-	form = ExampleForm()
-	if form.validate_on_submit():
-		print("salvando os dados:")
-		print(form.title.data)
-		print(form.content.data)
-		print(form.date.data)
-		flash('Dados salvos!')
-	return render_template('new.html', form=form)
+# @app.route('/save/', methods = ['GET','POST'])
+# @login_required
+# def save():
+# 	form = ExampleForm()
+# 	if form.validate_on_submit():
+# 		print("salvando os dados:")
+# 		print(form.title.data)
+# 		print(form.content.data)
+# 		print(form.date.data)
+# 		flash('Dados salvos!')
+# 	return render_template('new.html', form=form)
 
-@app.route('/view/<id>/')
-def view(id):
-	return render_template('view.html')
+# @app.route('/view/<id>/')
+# def view(id):
+# 	return render_template('view.html')
 
 # === User login methods ===
 
